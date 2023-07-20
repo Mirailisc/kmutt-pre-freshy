e37bd2a2cd391022cf745d52d12db6e8d2f9b697
Phubordin Poolnai <mirailisclm@gmail.com>
2023-07-20 15:56:17 +0700

    😭

--- examples/nested loop/loop_2.c
+++ examples/nested loop/loop_2.c
@@ -24,11 +24,15 @@ int main()
 
     for (int i = rows - 1; i >= 1; i--)
     {
-        for (int j = 1; j <= rows - i; j++)
+        for (int k = 1; k <= rows - i; k++)
+        {
             printf(" ");
+        }
 
         for (int j = 1; j <= 2 * i - 1; j++)
+        {
             printf("*");
+        }
 
         printf("\n");
     }
